97eeb7bd97072f891d49c5f9489cf11f834365f1
berengma <Gundul@mailbox.org>
2017-11-01 18:19:33 +0100

    added a basic leaderboard

--- init.lua
+++ init.lua
@@ -1,19 +1,78 @@
 
 local S = technic.getter
 
+local storage = minetest.get_mod_storage()
+local score = {}
 
+local adminname               = "Gundul" -- Admins do not have highscores !
 local cleanup_max_charge      = 30000 -- Maximum charge of the cleaner
 local enable_drops            = true  -- enable drops or not
 local cleanup_charge_per_node = 5     -- energy used to kill one sunflower
 local car = 20                        -- cleanup_action_radius
 local chatmessage             = true
 
-
-
-
-
 technic.register_power_tool("antipest:cleanup", cleanup_max_charge)
 
+
+-- load scoreboard
+local function openlist()
+
+	local load = storage:to_table()
+	score = load.fields
+	
+	for count in pairs(score) do
+	score[count] = tonumber(score[count])
+	end
+    
+end
+
+
+-- save scoreboard
+local function savelist()
+
+	storage:from_table({fields=score})
+	
+end -- poi.save()
+
+
+function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
+
+local function sortscore()
+    local fname = "size[5,6]"
+    local count = 1
+
+      for k,v in spairs(score, function(t,a,b) return t[b] < t[a] end) do
+	  --minetest.chat_send_all(count.." >>> "..k.." , "..v)
+	  fname = fname.."label[1,"..(count*0.3)..";"..count.." >>> "..k.." , "..v.."]"
+	  count = count + 1
+	  if count > 10 then break end
+      end
+  
+    fname = fname.."button_exit[1.5,5;2,1;quit;Exit]"
+    return fname
+end
 
 
 local function cleanup_dig(pos, current_charge, user)
@@ -23,11 +82,6 @@ local function cleanup_dig(pos, current_charge, user)
 	local counter = 0
 
 	local poslist = minetest.find_nodes_in_area(minp, maxp, {"flowers:sunflower"})
-
-	-- this debugs positions
-	-- minetest.chat_send_player(name, " you hit : "..pos.x.." , "..pos.y.." , "..pos.z)
-	-- minetest.chat_send_player(name, " minp    : "..minp.x.." , "..minp.y.." , "..minp.z)
-	-- minetest.chat_send_player(name, " maxp    : "..maxp.x.." , "..maxp.y.." , "..maxp.z)
 
 		for _,cpos in pairs(poslist) do
 
@@ -41,7 +95,7 @@ local function cleanup_dig(pos, current_charge, user)
 				counter = counter +1
 			   else
 			
-			   break
+			      break
 
 			   end
 
@@ -56,9 +110,17 @@ local function cleanup_dig(pos, current_charge, user)
 		if chatmessage then
 
 		minetest.chat_send_player(name, "You just killed "..counter.." evil sunflowers")
-
+		        
 		end
-
+		
+		if name ~= adminname then
+		      if score[name] then
+				score[name] = score[name] + counter
+			    else
+				score[name] = counter
+		      end
+		end
+		      
 		if enable_drops then
 			local inv = user:get_inventory()
 			local blocks = math.floor(counter/81)
@@ -71,6 +133,7 @@ local function cleanup_dig(pos, current_charge, user)
 
 	end
 
+	savelist()
 	return current_charge
 			
 				
@@ -140,4 +203,25 @@ minetest.register_craft({
 		{"technic:stainless_steel_ingot", "technic:red_energy_crystal", "technic:stainless_steel_ingot"},
 	}
 })
+
+minetest.register_chatcommand("score", {
+	params = "",
+	description = "Shows the best sunflower killer",
+	privs = {interact = true},
+	func = function(name)
+
+	    local fname = sortscore()
+	    if fname then
+	      --minetest.chat_send_player(name, ">>> Highscore is :"..score[highscore].." by "..highscore)
+	      minetest.show_formspec(name, "antipest:the_killers", fname)
+	    end
+
+	end,
+})
+
+-- Go and get them :D
+
+openlist()
+
+
 
